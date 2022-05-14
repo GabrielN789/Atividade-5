a8b51c47c9fe96b79ffef9b02ecfa8ca63f4cf04
leirb <leirb@DESKTOP-93I3BRU>
2022-05-14 15:55:11 -0300

    Adicionar arquivos de projeto.

---
+++ Atividade 5/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Atividade_5.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Atividade_5.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Exercicio 1";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Exercicio 2";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Exercicio 3";
+
+            return View();
+        }
+        public ActionResult Exercicio4()
+        {
+            ViewBag.Message = "Exercicio 4";
+
+            return View();
+        }
+        public ActionResult Exercicio5()
+        {
+            ViewBag.Message = "Exercicio 5";
+
+            return View();
+        }
+        public ActionResult Exercicio6()
+        {
+            ViewBag.Message = "Exercicio 6";
+
+            return View();
+        }
+        public ActionResult Exercicio7()
+        {
+            ViewBag.Message = "Exercicio 7";
+
+            return View();
+        }
+        public ActionResult Exercicio8()        {
+            CadastroUsuario user = new CadastroUsuario();
+            return View(user);
+        }
+        [HttpPost]
+        public ActionResult exibeCadastro(CadastroUsuario user)
+        {            
+                return View(user);                    
+        }
+
+    }
+}
---
+++ Atividade 5/Models/CadastroUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Atividade_5.Models
+{
+    public class CadastroUsuario
+    {   [RegularExpression(@"^([a-zA-Z]{2,}\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\s?([a-zA-Z]{1,})?)", ErrorMessage = "Nome inválido" )]
+        [Required(ErrorMessage = "O campo nome é obrigatório")]
+        [MinLength(5, ErrorMessage = "Nome deve ser maior que 5 caracteres")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo telefone é obrigatório")]
+        [RegularExpression(@"^\(?[1-9]{2}\)? ?(?:[2-8]|9[1-9])[0-9]{3}\-?[0-9]{4}$", ErrorMessage = "Telefone inválido")]
+        public string Telefone { get; set; }
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "E-mail inválido")]
+        [Required(ErrorMessage = "O campo e-mail é obrigatório")]        
+        public string Email { get; set; }
+    }
+}
